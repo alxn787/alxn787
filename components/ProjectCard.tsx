3e29ca37cfb675fec32f2da2c8c64a93536c37f0
alxn787 <alenabraham787@gmail.com>
2025-06-13 14:39:40 +0530

    ui

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -24,14 +24,19 @@ export default function ProjectCard({
       onClick={()=>handleOpenInNewTab(url)}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className="relative flex flex-col items-center justify-between  rounded-xl p-10 transition-all duration-200 hover:-translate-y-1 hover:shadow-md dark:hover:shadow-lg min-w-[16rem] min-h-[16rem] w-full h-full overflow-hidden cursor-pointer "
+      className="relative flex flex-col items-center justify-between  rounded-xl p-10 transition-all duration-200 hover:-translate-y-1 hover:shadow-md dark:hover:shadow-lg min-w-[16rem] min-h-[16rem] w-full h-full overflow-hidden cursor-pointer hover:scale-105 "
     >
-      <div
-        className={`absolute inset-0 bg-center bg-cover transition-all duration-500 transform rounded-lg z-0 ease-in-out
+      {/* <div
+        className={`absolute inset-0 bg-center bg-cover  transition-all duration-500 transform rounded-lg z-0 ease-in-out
           ${isHovered ? 'opacity-100 scale-105' : 'opacity-0 scale-100'}
         `}
         style={{ backgroundImage: `url(${Imageurl})`}}
 
+      /> */}
+        <img
+        src={Imageurl}
+        alt={Title}
+        className={`absolute inset-0 w-full h-full object-contain transition-all duration-500 ${isHovered ? 'opacity-100 scale-110' : 'opacity-0 scale-95'}`}
       />
 
         <div
